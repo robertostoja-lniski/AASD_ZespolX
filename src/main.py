15265b1c3d00c5d4e86630bda28ec2c439e795f6
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2021-12-17 17:01:31 +0100

    tidying

--- src/main.py
+++ src/main.py
@@ -15,48 +15,6 @@ from src.agents.weather_monitoring import WeatherMonitoring
 from src.mas_logging import create_logger
 
 
-class SenderAgent(Agent):
-    class InformBehav(OneShotBehaviour):
-        async def run(self):
-            print("InformBehav running")
-            msg = Message(to="receiver@localhost")     # Instantiate the message
-            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
-            msg.body = "Hello World"                    # Set the message content
-
-            await self.send(msg)
-            print("Message sent!")
-
-            # stop agent from behaviour
-            await self.agent.stop()
-
-    async def setup(self):
-        print("SenderAgent started")
-        b = self.InformBehav()
-        self.add_behaviour(b)
-
-class ReceiverAgent(Agent):
-    class RecvBehav(OneShotBehaviour):
-        async def run(self):
-            print("RecvBehav running")
-
-            msg = await self.receive(timeout=20) # wait for a message for 10 seconds
-            if msg:
-                print("Message received with content: {}".format(msg.body))
-            else:
-                print("Did not received any message after 10 seconds")
-
-            # stop agent from behaviour
-            await self.agent.stop()
-
-    async def setup(self):
-        print("ReceiverAgent started")
-        b = self.RecvBehav()
-        template = Template()
-        template.set_metadata("performative", "inform")
-        self.add_behaviour(b, template)
-
-
-
 if __name__ == "__main__":
     agents = []
     logger = create_logger('main')
