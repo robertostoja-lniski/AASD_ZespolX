122426710fbf95fc8da7310f6d623dc136ca5e26
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2021-12-17 14:29:32 +0100

    agent users2

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
-prosodyctl register test localhost 1qaz@WSX
-prosodyctl register sender localhost 1qaz@WSX
+prosodyctl register test1 localhost 1qaz@WSX
+prosodyctl register test2 localhost 1qaz@WSX
+prosodyctl register test3 localhost 1qaz@WSX
 prosodyctl register receiver localhost 1qaz@WSX
+prosodyctl register sender localhost 1qaz@WSX
+
+prosodyctl register water_monitoring localhost 1qaz@WSX
+prosodyctl register fish_content_monitoring localhost 1qaz@WSX
+prosodyctl register weather_monitoring localhost 1qaz@WSX
+prosodyctl register crowdiness_monitoring localhost 1qaz@WSX
+prosodyctl register data_accumulator localhost 1qaz@WSX
+prosodyctl register fishery_recommender localhost 1qaz@WSX
+prosodyctl register client_reporter localhost 1qaz@WSX
+prosodyctl register user localhost 1qaz@WSX
